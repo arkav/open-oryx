92afe674c17e6faffae4a6fba30fda27acfb8074
arkav <arkisadev@gmail.com>
2019-10-26 16:30:06 -1000

    1.1.0

--- update.bat
+++ update.bat
@@ -1,10 +1,10 @@
 @echo off
 echo Downloading version.txt
-curl https://mysterious-inlet-24356.herokuapp.com/txt/version -o target\version.txt
+curl https://mysterious-inlet-24356.herokuapp.com/txt/version -o build\version.txt
 echo Downloading packets.xml
-curl https://mysterious-inlet-24356.herokuapp.com/xml/packets -o target\packets.xml
+curl https://mysterious-inlet-24356.herokuapp.com/xml/packets -o build\packets.xml
 echo Downloading objects.xml
-curl https://static.drips.pw/rotmg/production/current/xmlc/Objects.xml -o target\objects.xml
+curl https://static.drips.pw/rotmg/production/current/xmlc/Objects.xml -o build\objects.xml
 echo Downloading tiles.xml
-curl https://static.drips.pw/rotmg/production/current/xmlc/GroundTypes.xml -o target\tiles.xml
+curl https://static.drips.pw/rotmg/production/current/xmlc/GroundTypes.xml -o build\tiles.xml
 echo Done!
